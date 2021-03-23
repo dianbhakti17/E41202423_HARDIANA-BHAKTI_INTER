0c995554c46bf697e68ab8e3d068e17b5f0cf215
dianbhakti17 <80333972+dianbhakti17@users.noreply.github.com>
2021-03-23 14:31:33 +0700

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array1;
+
+/**
+ *
+ * @author Dian
+ */
+public class Array1 {
+    public static void main(String[] args) {
+          int[] angka = {3, 7, 8, 1, 10, 20, 25};
+          
+          System.out.println("Jumlah elemen array angka = " +angka.length);
+    }
+    
+}
---
+++ Variableproblem.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variableproblem;
+
+/**
+ *
+ * @author Dian
+ */
+public class Variableproblem {
+
+    public static void main(String[] args) {
+        
+        //Variable yang benar
+        boolean gameOver = false;
+        int studwnts = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        //variable yang salah
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+        
+        // TODO code application logic here
+    }
+    
+}
